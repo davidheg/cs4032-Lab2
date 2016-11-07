f9264c631a10f16726100b5f5e3b68a6b0d9fb56
davidheg <david.hegarty.9@gmail.com>
2016-11-07 17:13:46 +0000

    Accidently uploaded incomplete server, this is the working version

--- server.py
+++ server.py
@@ -10,10 +10,11 @@ def openSocket(address,port):
 def main():
     address = "10.62.0.213"
     port = 8000
-    #openSocket(address,port);
-    sock = socket.socket()
-    sock.listen()
-    data = sock.recv(1024)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((address,port))
+    sock.listen(10)
+    conn, addr = sock.accept()
+    data = conn.recv(1024)
     print data
-    
-if __name__ == "__main__":main() ## with if
+ 
+main()
