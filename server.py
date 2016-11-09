8ec9d3425bd06573464b473f37393a13fa9a5b45
davidheg <david.hegarty.9@gmail.com>
2016-11-09 16:16:44 +0000

    Updated server that creates threads to handle clients

--- server.py
+++ server.py
@@ -1,48 +1,35 @@
 import socket
-import sys
 import threading
-from threading import Thread
+import os
 
 run = True
 max = 10
-threadPool = []
-threadNumber = 0
 address = "10.62.0.213"
 port = 8000
-StudentID = 13332863
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def createThread(data):
-    if len(threadPool) < max:
-        thread = Thread(target = handleClient, args = [data]);
-        threadPool.append(thread)
-        thread.start()
-    else:
-        i = 0;
-        while( i < len(threadPool)):
-            thread = threadPool[i]
-            if(not(thread.isAlive())):
-                threadPool.pop(i)
-        createThread(data)
-    return
 
-def handleClient(data):
-    print "threads number = ", threading.activeCount()
-    if data == "HELO text\n":
-        print "HELO text\nIP:%s\nPort:%d\nStudentID:%d\n" %(address,port,StudentI$
-    elif data == "KILL_SERVICE\n":
-        global run
-        run = False;
-        print "Killing service"
-    else:
-        print data
-                              
-def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((address,port))
-    while(run):
-        sock.listen(100)
-        conn, addr = sock.accept()
+def handleClient(number,conn):
+    receiving = True
+    while receiving:
         data = conn.recv(1024)
-        createThread(data)
+        if data == "HELO text\n":
+            print number, "HELO text\nIP:%s\nPort:%d\nStudentID:13332863\n" %(address,port)
+        elif data == "KILL_SERVICE\n":
+            print "Killing Service"
+            sock.close()
+            print "Socket closed"
+            os._exit(1)
+        elif not data:
+            receiving = False
+        else:
+            print number, data
 
-main()
+
+sock.bind((address,port))
+print "Socket created at IP:%s and port:%s, now listening for clients" %(address,port)
+sock.listen(5)
+
+while run:
+    conn, addr = sock.accept()
+    threading.Thread(target = handleClient, args =(current,conn,)).start() 
