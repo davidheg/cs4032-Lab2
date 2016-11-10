20957c6718a1f61c290297cff07fdbea6f23396c
davidheg <david.hegarty.9@gmail.com>
2016-11-10 01:11:38 +0000

    Updated server, made several fixes

--- server.py
+++ server.py
@@ -1,20 +1,21 @@
 import socket
 import threading
 import os
+import sys
 
 run = True
 max = 10
 address = "10.62.0.213"
-port = sys.argv[1]
+port = int(sys.argv[1])
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-def handleClient(number,conn):
+def handleClient(conn,addr):
     receiving = True
     while receiving:
         data = conn.recv(1024)
-        if data == "HELO text\n":
-            print number, "HELO text\nIP:%s\nPort:%d\nStudentID:13332863\n" %(address,port)
+        if "HELO" in data:
+            conn.send("%sIP:%s\nPort:%d\nStudentID:13332863\n" %(data,address,port))
         elif data == "KILL_SERVICE\n":
             print "Killing Service"
             sock.close()
@@ -23,12 +24,12 @@ def handleClient(number,conn):
         elif not data:
             receiving = False
         else:
-            print number, data
-
+            print data
 
 sock.bind((address,port))
-print "Socket created at IP:%s and port:%s, now listening for clients" %(address,port)
+print "Socket created at IP:%s and port:%d, now listening for clients" %(address,port)
 sock.listen(5)
 while run:
     conn, addr = sock.accept()
-    threading.Thread(target = handleClient, args =(current,conn,)).start() 
+    threading.Thread(target = handleClient, args =(conn,addr,)).start()
+
